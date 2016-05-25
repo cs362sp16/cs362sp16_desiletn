80c455b97f58fc368526782eacfadc69732f96b3
Nic Desilets <nic@desilets.org>
2016-05-24 19:55:46 -0700

    Semi-working testdominion

--- dominion/tests/testdominion.c
+++ dominion/tests/testdominion.c
@@ -7,39 +7,72 @@
 #include <time.h>
 #include "../dominion.h"
 
-#define ITERATIONS 2
+#define ITERATIONS 10
 #define K_MIN adventurer
 #define K_MAX treasure_map
 
 int _CARD[] = {
     curse,
-    estate,
+    estate, // 1
     duchy,
-    province,
+    province, // 3
     copper,
-    silver,
+    silver, // 5
     gold,
-    adventurer,
+    adventurer, // 7
     council_room,
-    feast,
+    feast, // 9
     gardens,
-    mine,
+    mine, // 11
     remodel,
-    smithy,
+    smithy, // 13
     village,
-    baron,
+    baron, // 15
     great_hall,
-    minion,
+    minion, // 17
     steward,
-    tribute,
+    tribute, // 19
     ambassador,
-    cutpurse,
+    cutpurse, // 21
     embargo,
-    outpost,
+    outpost, // 23
     salvager,
-    sea_hag,
+    sea_hag, // 25
     treasure_map
 };
+
+char* enumToStr(int card){
+    switch(card){
+        case adventurer: return "Adventurer";
+		case council_room: return "Council Room";
+		case feast: return "Feast";
+		case great_hall: return "Great Hall";
+		case mine: return "Mine";
+		case remodel: return "Remodel";
+		case smithy: return "Smithy";
+		case curse: return "Curse";
+		case estate: return "Estate";
+		case duchy: return "Duchy";
+		case ambassador: return "Ambassador";
+		case cutpurse: return "Cutpurse";
+		case embargo: return "Embargo";
+		case outpost: return "Outpost";
+		case salvager: return "Salvager";
+		case sea_hag: return "Sea Hag";
+		case province: return "Province";
+		case copper: return "Copper";
+		case silver: return "Silver";
+		case gold: return "Gold";
+		case gardens: return "Gardens";
+		case village: return "Village";
+		case baron: return "Baron";
+		case minion: return "Minion";
+		case steward: return "Steward";
+		case tribute: return "Tribute";
+		case treasure_map: return "Treasure Map";
+		default: return "ya dun goof'd";
+    }
+}
 
 // Fill deck with unique cards
 void initDeck(int *cards){
@@ -68,16 +101,20 @@ void initGameParams(int *numPlayers, int *cards, int seed, struct gameState *sta
     initializeGame(*numPlayers, cards, seed, state);
 }
 
-// Play a game of dominion until finished
-void playDom(struct gameState *state){
+// Play round
+void playRound(struct gameState *state){
     int player = whoseTurn(state);
 
-    // Play card phase
-    while(state->numActions){
-        printf("While Action\n");
+    //printf("ENTER: while(state->numActions)\n");
+    while(state->numActions > 0){
+        //printf("IN: while(state->numActions)\n");
         int actionCards = 0;
         int handPos = 0;
         int card = 0;
+        int cashMoney = 0;
+
+        /* Play card phase */
+        //printf("Play phase\n");
 
         // Calculate number of actions available
         for(int i = 0; i < numHandCards(state); i++){
@@ -88,46 +125,70 @@ void playDom(struct gameState *state){
 
         // If actions available
         if(actionCards > 0){
+            //printf("Action available\n");
             // Pick random card in hand
             handPos = rand() % state->handCount[player];
             card = state->hand[player][handPos];
 
+            //printf("Actions: %i\n", actionCards);
+            //printf("Card: %s\n", enumToStr(card));
+
             // If kingdom card
             if(card >= K_MIN && card <= K_MAX){
-                printf("Card: %i\n", card);
+                //printf("Is kingdom card\n");
                 switch(card){
+                    case ambassador:
+                        playCard(handPos, (rand() % numHandCards(state)), 1, -1, state);
+                        break;
+                    case baron:
+                        playCard(handPos, 0, -1, -1, state);
+                        break;
+                    case embargo:
+                        playCard(handPos, (rand() % (1 - 26) + 26), -1, -1, state);
+                        break;
                     case feast:
-                        playCard(handPos, )
-                    case mine:
-                        playCard(handPos, copper, silver, 0, state);
+                        //playCard(handPos, (rand() % (duchy + 1 - 26) + 26), -1, -1, state);
+                        break;
+                    case minion:
+                        playCard(handPos, 1, -1, -1, state);
                         break;
                     case remodel:
-                        playCard(handPos, copper, estate, 0, state);
+                        playCard(handPos, (rand() % numHandCards(state)), (rand() % (estate + 1 - 26) + 26), -1, state);
+                        playCard(handPos, (rand() % numHandCards(state)), -1, -1, state);
                         break;
                     case steward:
-                        playCard(handPos, 2, 0, 0, state);
+                        playCard(handPos, ((rand() % 3) + 1), -1, -1, state);
                         break;
                     default:
                         playCard(handPos, 0, 0, 0, state);
                         break;
                 }
+                actionCards--;
+            }else{
+                //printf("Is NOT kingdom card\n");
             }
         }else{
-            break;
+            //printf("Action NOT available\n");
         }
-    }
 
-    // Play buy phase
-    updateCoins(player, state, NULL);
-    while(state->coins && state->numBuys){
-        printf("While Buy\n");
-        int tmpCard;
-        do{
-            tmpCard = rand() % 27;
-        }while(buyCard(tmpCard, state) != 0);
-    }
+        /* Play buy phase */
+        //printf("Buy phase\n");
 
-    endTurn(state);
+        updateCoins(player, state, NULL);
+        cashMoney = state->coins;
+        while(cashMoney > 0){
+            //printf("cashMoney && state->numBuys)\n");
+            int tmpCard = rand() % 27;
+            int cost = getCost(tmpCard);
+
+            if(cost <= cashMoney){
+                cashMoney = buyCard(tmpCard, state);
+            }
+        }
+
+        endTurn(state);
+        break;
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -144,7 +205,6 @@ int main(int argc, char *argv[]){
 
     printf("[Beginning Dominion tests]\n");
     for(int i = 0; i < ITERATIONS; i++){
-        printf("test\n");
         if(i == 0){
             printf("%0.2f%% complete.", ((i + 1) / (float)ITERATIONS) * 100);
         }else{
@@ -159,11 +219,11 @@ int main(int argc, char *argv[]){
 
         // Play until finished
         while(!isGameOver(&state)){
-            playDom(&state);
+            playRound(&state);
         }
     }
 
-    printf("\n");
+    //printf("\n");
 
     return 0;
 }
